d03e1013ae7728474f5fbd8547ebbed57c2f5da9
rickywychoi <cchaff0105@gmail.com>
2021-01-31 09:25:08 -0800

    End of adding posts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,46 +1,41 @@
-import React from 'react';
-import { gql, useQuery } from '@apollo/client';
-import { Grid } from 'semantic-ui-react';
+import React, { useContext } from 'react'
+import { useQuery } from '@apollo/client'
+import { Grid } from 'semantic-ui-react'
 
-import PostCard from '../containers/PostCard';
+import { AuthContext } from '../context/auth'
+import PostCard from '../containers/PostCard'
+import PostForm from '../containers/PostForm'
+import { FETCH_POSTS_QUERY } from '../util/graphql'
 
-function Home() {
-  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
-  const posts = data && data.getPosts;
-  
+function Home () {
+  const { user } = useContext(AuthContext)
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY)
+  const posts = data && data.getPosts
+
   return (
     <Grid columns={3}>
-      <Grid.Row className="page-title">
+      <Grid.Row className='page-title'>
         <h1>Recent Posts</h1>
       </Grid.Row>
       <Grid.Row>
+        {user && (
+          <Grid.Column>
+            <PostForm />
+          </Grid.Column>
+        )}
         {loading ? (
           <h1>Loading posts...</h1>
         ) : (
-          posts && posts.map(post => (
+          posts &&
+          posts.map(post => (
             <Grid.Column key={post.id} style={{ marginBottom: '20px' }}>
-              <PostCard post={post}/>
+              <PostCard post={post} />
             </Grid.Column>
           ))
         )}
       </Grid.Row>
     </Grid>
-  );
+  )
 }
 
-const FETCH_POSTS_QUERY = gql`
-  query {
-    getPosts {
-      id body createdAt username likeCount
-      likes {
-        username
-      }
-      commentCount
-      comments {
-        id username createdAt body
-      }
-    }
-  }
-`;
-
-export default Home;
+export default Home
